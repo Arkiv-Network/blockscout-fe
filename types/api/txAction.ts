6de21da73c3273e9b79e51ff22b21c797a5a6722
kamirr <koczurekk@gmail.com>
2025-05-13 15:31:10 +0200

    feat: implement support for GolemBase tx actions

--- types/api/txAction.ts
+++ types/api/txAction.ts
@@ -21,6 +21,32 @@ export interface TxActionNft {
   };
 }
 
-export type TxAction = {
+export type TxGolemBaseAction = {
+  type: 'golembase_entity_created' | 'golembase_entity_updated';
+  data: {
+    entity_id: string;
+    expiration_block: number;
+  };
+} | {
+  type: 'golembase_entity_deleted';
+  data: {
+    entity_id: string,
+  };
+} | {
+  type: 'golembase_entity_ttl_extended';
+  data: {
+    entity_id: string,
+    old_expiration_block: number,
+    new_expiration_block: number,
+  }
+}
+
+export type TxActionUniswap = {
   protocol: 'uniswap_v3';
 } & (TxActionGeneral | TxActionNft);
+
+export type TxActionGolemBase = {
+  protocol: 'golembase';
+} & TxGolemBaseAction;
+
+export type TxAction = TxActionUniswap | TxActionGolemBase;
